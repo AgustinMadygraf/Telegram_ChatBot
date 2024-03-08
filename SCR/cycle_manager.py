dc4bc1edfbe1eef5927959d0c1ef05a7d4569172
AgustinMadygraf <agustin.mtto.madygraf>
2024-03-08 20:20:27 -0300

    Add chat session and message processing cycle

--- SCR/cycle_manager.py
+++ SCR/cycle_manager.py
@@ -4,39 +4,51 @@ from bot.processing import cargar_chat_history, procesar_respuesta
 from bot.bot import send_a
 import logging
 import time
+import asyncio
+from logs.config_logger import configurar_logging
+
+logger = configurar_logging()
 
 async def ejecutar_ciclo_principal(model, config, user_id_str, seleccion_modelo, respuesta_rapida):
     """
     Ejecuta el ciclo principal de procesamiento de mensajes para el chatbot.
-
-    Esta función maneja el flujo principal de interacción con el usuario a través del chatbot. Incluye la recepción 
-    de mensajes, el procesamiento de respuestas y el envío de respuestas. Utiliza un modelo de IA para generar respuestas.
-
-    Parámetros:
-    model (GPT4All): Modelo de IA utilizado para generar respuestas.
-    config (dict): Configuración del bot, incluyendo las plantillas del sistema.
-    user_id_str (str): Identificador del usuario como cadena.
-    seleccion_modelo (str): Modelo de IA seleccionado.
-    respuesta_rapida (bool): Indica si se debe utilizar una respuesta rápida o detallada.
-
-    Retorna:
-    None
     """
-    i = 2
-    seg = 5
+    await iniciar_sesion_chat(model, config)
+    await ciclo_de_procesamiento(model, config, user_id_str, seleccion_modelo, respuesta_rapida)
+
+async def iniciar_sesion_chat(model, config):
+    """
+    Inicia sesión de chat con el modelo de IA utilizando la plantilla de sistema.
+    """
     with model.chat_session(config['system_templates'][1]['template']):
-        while True:
-            await TelegramChatArchiver.main()
-            chat_history, user_info, ultimo_rol = cargar_chat_history(config['chat_history_path'])
-            logging.info(f"Último mensaje en la conversación: rol '{ultimo_rol}'")
-            if ultimo_rol == "user":     
-                seg = 5
-                await send_a(593052206)  
-                await procesar_respuesta(chat_history, user_id_str, model, i, user_info, seleccion_modelo, config['telegram_token'], respuesta_rapida)
-            else:
-                logging.info("Esperando consulta desde Telegram")
-                for seg_temp in range(seg, 0, -1):
-                    print(f"Continuando en {seg_temp} segundos...", end="\r")
-                    time.sleep(1)
-                if seg < 6:
-                    seg += 1
+        pass  # Aquí puedes incluir cualquier lógica inicial específica de la sesión si es necesario
+
+async def ciclo_de_procesamiento(model, config, user_id_str, seleccion_modelo, respuesta_rapida):
+    """
+    Ciclo continuo que procesa los mensajes entrantes y responde.
+    """
+    seg = 5
+    while True:
+        await obtener_y_procesar_mensajes(model, config, user_id_str, seleccion_modelo, respuesta_rapida, seg)
+        seg = await manejo_espera(seg)
+
+async def obtener_y_procesar_mensajes(model, config, user_id_str, seleccion_modelo, respuesta_rapida, seg):
+    """
+    Obtiene y procesa mensajes de Telegram.
+    """
+    await TelegramChatArchiver.main()
+    chat_history, user_info, ultimo_rol = cargar_chat_history(config['chat_history_path'])
+    logging.info(f"Último mensaje en la conversación: rol '{ultimo_rol}'")
+    if ultimo_rol == "user":
+        await send_a(593052206)
+        await procesar_respuesta(chat_history, user_id_str, model, 2, user_info, seleccion_modelo, config['telegram_token'], respuesta_rapida)
+
+async def manejo_espera(seg):
+    """
+    Maneja la espera entre la recepción de mensajes.
+    """
+    logging.info("Esperando consulta desde Telegram")
+    for seg_temp in range(seg, 0, -1):
+        print(f"Continuando en {seg_temp} segundos...", end="\r")
+        await asyncio.sleep(1)
+    return min(seg + 1, 6)  # Aumenta el tiempo de espera hasta un máximo de 6 segundos
